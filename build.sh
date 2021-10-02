0d2b3ad84fbe7aaad99b56114966887919ba888f
i02bahip <44676534+i02bahip@users.noreply.github.com>
2021-10-02 21:45:14 +0200

    Only one link file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 environment/compiler/wla-dx/bin/linux/wla-z80 -v -o out/main.o src/main.asm
-environment/compiler/wla-dx/bin/linux/wlalink -d -v -s environment/compiler/wla-dx/bin/linux/link.lk out/main.sms
+environment/compiler/wla-dx/bin/linux/wlalink -d -v -s environment/compiler/wla-dx/link/link.lk out/main.sms
 rm out/*.o
